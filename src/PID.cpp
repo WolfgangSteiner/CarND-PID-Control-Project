809648b50cfc6cf1bbe1bee775ac98b3d9890912
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2017-04-26 19:16:55 +0200

    first working version

--- src/PID.cpp
+++ src/PID.cpp
@@ -6,16 +6,33 @@ using namespace std;
 * TODO: Complete the PID class.
 */
 
-PID::PID() {}
+PID::PID()
+: p_error_(0.0)
+, i_error_(0.0)
+, d_error_(0.0)
+, last_cte_(0.0)
+, total_cte_(0.0)
+{}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
+void PID::Init(double aKp, double aKi, double aKd)
+{
+  Kp_ = aKp;
+  Ki_ = aKi;
+  Kd_ = aKd;
 }
 
-void PID::UpdateError(double cte) {
+void PID::UpdateError(double cte)
+{
+  p_error_ = cte;
+  d_error_ = (cte - last_cte_);
+  total_cte_ += cte;
+  last_cte_ = cte;
 }
 
-double PID::TotalError() {
+double PID::TotalError()
+{
+  return Kp_ * p_error_ + Kd_ * d_error_ + Ki_ * i_error_;
 }
 
